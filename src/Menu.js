619e8cd1aa37b65d6a7a586398c163094e948296
crazycamaro68 <153693685+crazycamaro68@users.noreply.github.com>
2024-08-15 21:04:35 -0400

    Made Menu page and styled it. Also checked to make sure page switching is working and it is.

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,5 @@
 export function menu(){
+    //Title Generator
     const container = document.getElementById('content');
     let titleContainer = document.createElement('div');
     titleContainer.setAttribute("id","titleContainer");
@@ -6,4 +7,43 @@ export function menu(){
     title.textContent = "Bucks Stars"
     container.appendChild(titleContainer);
     titleContainer.appendChild(title);
+
+    let menuContainer = document.createElement("div");
+    let menuContainerTitle = document.createElement("p");
+    menuContainerTitle.textContent = "Menu";
+    menuContainerTitle.setAttribute("id","menuContainerTitle");
+    menuContainer.setAttribute("id","menuContainer");
+    container.appendChild(menuContainer);
+    menuContainer.appendChild(menuContainerTitle);
+
+    for(let i = 0; i < menuItemsContainer.length; i++){
+        let itemName = document.createElement("p");
+        let itemPrice = document.createElement("p");
+        let itemDescribtion = document.createElement("p");
+        let itemContainer = document.createElement("div");
+        itemContainer.setAttribute("id", `${menuItemsContainer[i].itemName}`);
+        itemContainer.setAttribute("class","menuItems")
+        itemName.textContent = menuItemsContainer[i].itemName;
+        itemPrice.textContent = menuItemsContainer[i].itemPrice;
+        itemDescribtion.textContent = menuItemsContainer[i].itemDescribtion;
+        menuContainer.appendChild(itemContainer);
+        itemContainer.appendChild(itemName);
+        itemContainer.appendChild(itemPrice);
+        itemContainer.appendChild(itemDescribtion);
+    }
+
 }
+
+function menuItems(name, price, description){
+    const itemName = name;
+    const itemPrice = price;
+    const itemDescribtion = description;
+
+    return {itemName, itemPrice, itemDescribtion}
+}
+let menuItemsContainer = [];
+let coffee = menuItems("Coffee", 2.99, "Coffee anyway you want it!")
+let bagle = menuItems("Bagle", 1.99,"Warm Bagle");
+let latte = menuItems("Latte", 2.99,"Warm Latte");
+menuItemsContainer.push(coffee, bagle, latte);
+console.log(menuItemsContainer);
